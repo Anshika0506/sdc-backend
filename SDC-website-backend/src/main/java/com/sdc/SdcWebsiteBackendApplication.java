e0ed72bf692e08587af3f042fc59a2f9a6976c43
Devansh-solanki95 <devanshsolanki83@gmail.com>
2025-06-26 10:15:10 +0530

    initial commit making of project

---
+++ SDC-website-backend/src/main/java/com/sdc/SdcWebsiteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdcWebsiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdcWebsiteBackendApplication.class, args);
+	}
+
+}
