debc3ded8cfe014347cc882fd84dd519d027b864
AyushiChoudhary27 <Ayushichoudhary2786@gmail.com>
2025-06-27 10:02:45 +0530

    Admin.java

---
+++ SDC-website-backend/src/main/java/com/sdc/entity/Admin.java
@@ -0,0 +1,67 @@
+package com.sdc.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "admin")
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long adminId;
+    private String name;
+    private String email;
+    private String contactNo;
+    private String password;
+
+    // Constructors
+    public Admin() {}
+
+    public Admin(String name, String email, String contactNo, String password) {
+        this.name = name;
+        this.email = email;
+        this.contactNo = contactNo;
+        this.password = password;
+    }
+
+    // Getters & Setters
+    public Long getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(Long adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContactNo() {
+        return contactNo;
+    }
+
+    public void setContactNo(String contactNo) {
+        this.contactNo = contactNo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
