3f0e6744d6385ef3002fbac55be30ef4ee8de778
AyushiChoudhary27 <Ayushichoudhary2786@gmail.com>
2025-06-30 09:33:53 +0530

    Admin create

---
+++ SDC-website-backend/src/main/java/com/sdc/entity/Admin.java
@@ -0,0 +1,33 @@
+package com.sdc.entity;
+
+import jakarta.persistence.*;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "admin")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Admin {
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long adminId;
+    
+    @Column
+    private String name;
+    
+    @Column
+    private String email;
+    
+    @Column
+    private String contactNo;
+    
+    @Column
+    private String password;
+
+}
