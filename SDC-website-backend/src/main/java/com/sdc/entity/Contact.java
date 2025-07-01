f7cfd7d5662e6a118c1d9bf19720e890e9ab7ed9
Mayuresh Khedkar <mayureshkhedkar05@gmail.com>
2025-07-01 14:24:39 +0530

    commit

--- SDC-website-backend/src/main/java/com/sdc/entity/Contact.java
+++ SDC-website-backend/src/main/java/com/sdc/entity/Contact.java
@@ -11,7 +11,7 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 
 public class Contact {
-	
+	// this is a test
 	    @Id
 	    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	    private Integer contactId;
