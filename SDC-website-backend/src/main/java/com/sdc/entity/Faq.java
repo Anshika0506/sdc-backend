dfe21ff128c23de0bd92f68b2914a09b8d8ca6e1
Devansh-solanki95 <devanshsolanki83@gmail.com>
2025-07-02 15:39:04 +0530

    faq and testimonial entity and their models are made

---
+++ SDC-website-backend/src/main/java/com/sdc/entity/Faq.java
@@ -0,0 +1,33 @@
+package com.sdc.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="faq")
+
+public class Faq {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column
+	private String ques;
+
+	@Column
+	private String ans;
+
+	public Faq(String ques, String ans) {
+		super();
+		this.ques = ques;
+		this.ans = ans;
+	}
+	
+	
+	
+}
---
+++ SDC-website-backend/src/main/java/com/sdc/models/TestimonialsModel.java
@@ -0,0 +1,16 @@
+package com.sdc.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TestimonialsModel {
+	
+	private String clientName;
+	
+	private String des;
+
+}
